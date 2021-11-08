64831190390ee46909bbd45d1cec0e1485d16886
yavuz-bektas <bektasy8@gmail.com>
2021-11-08 22:58:24 +0300

    feat(home.vue): main page display with local storage user auth

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <Navbar />
+
+    <v-main class="dark">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="12">
+            <v-sheet min-height="0vh" rounded="lg">
+              <Vcard />
+              <strong>{{ this.$store.state.select }}</strong> ile ilgili
+              sonuçlar listeleniyor.
+            </v-sheet>
+          </v-col>
+          <v-col v-for="i in $store.state.searchResults" :key="i.id" cols="4">
+            <v-card @click="push(i['1. symbol'])" height="400">
+              <div v-for="(value, key) in i" :key="key">
+                <v-btn class="mx-5 my-1" outlined color="white">
+                  {{ key }}
+                </v-btn>
+                <v-icon dark lef> mdi-arrow-right </v-icon>
+                <v-btn class="mx-5 my-1" outlined color="red">
+                  {{ value }}
+                </v-btn>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Navbar from "@/components/Navbar";
+import Vcard from "@/components/vcard";
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Vcard,
+  },
+
+  created(){
+    localStorage.setItem("areyouadmin","user");
+  },
+
+  methods: {
+    push(key) {
+      this.$router.push({
+        path: "/symbol",
+        query: {
+          symbol: key,
+        },
+      });
+    },
+  },
+};
 </script>
